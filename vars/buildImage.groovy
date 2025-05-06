37c56290503de13b5a5ed3e623b252468dc3b680
devsecopsaws1 <ravaluruvinodkumar@gmail.com>
2025-05-06 08:43:53 +0530

    added

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,15 +1,19 @@
 #!/usr/bin/env groovy
 
 // This is a Jenkins shared library function to build a Docker image using Docker
-def call() {
+def call( String imageName) {
     echo "Building Docker image..."
     withCredentials([
         usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')
     ]) {
-        sh 'docker build -t rameshm1/demo-java-app:1.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
-        sh 'docker push rameshm1/demo-java-app:1.0'
+        sh "docker push $imageName "
     }
     // Replace 'my-docker-image' with your desired image name   
 }
 
+
+//builImage 'rameshm1/demo-java-app:1.2.0'
+
+//filenameofgroovy.functioname
