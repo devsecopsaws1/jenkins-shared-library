900e693ef7ddc63d91a8e9d92d3c6a8418e33a95
devsecopsaws1 <ravaluruvinodkumar@gmail.com>
2025-05-06 08:29:51 +0530

    added

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 // This is a Jenkins shared library function to build a JAR file using Maven
-def call() {
+def myjar() {
     echo "Building JAR file..."
     sh 'mvn package'
 }
